9721f42eebea62a940abb2b768f5739bfa3212eb
rosspa <rosspa@rose-hulman.edu>
2016-02-26 21:21:38 -0800

    added endpoint for checking if a summoner is still in a game

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,6 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// You can use CoffeeScript in this file: http://coffeescript.org/
-
 $.ajax({
-    url: "/na/base", // Route to the Script Controller method
+    //url: "/na/CHATSPAMKAPPA123", // Route to the Script Controller method
+    url: "/na/baseballlover723", // Route to the Script Controller method
     type: "GET",
     dataType: "json",
     complete: function() {},
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,13 @@
 class MainController < ApplicationController
+  @@keys = ENV["RIOT_API_KEYS"].split(" ")
+  @@key_index = -1
+  @@request_base = ".api.pvp.net"
+  @@game_path = "/observer-mode/rest/consumer/getSpectatorGameInfo/"
+  @@id_path1 = "/api/lol/"
+  @@id_path2 = "/v1.4/summoner/by-name/"
+  @@region_translator = {br: "BR1", eune: "EUN1", euw: "EUW1", kr: "KR", lan: "LA1", las: "LA2",
+                         na: "NA1", oce: "OC1", ru: "RU", tr: "TR1"}
+
   def index
   end
 
@@ -6,26 +15,52 @@ class MainController < ApplicationController
     respond_to do |format|
       format.html
       format.json do
-        handle_json
+        handle_json(params[:region], params[:username])
       end
     end
   end
 
-  def handle_json
-    if rand(2) == 1
-      json = json_in_game
-    else
-      json = json_out_game
-    end
-    render json: json
+  def handle_json(region, username)
+    user_id = get_user_id region, username
+    return render json: json_invalid_summoner unless user_id
+    key = next_key
+    game_uri = URI::HTTPS.build(host: region + @@request_base,
+                                path: @@game_path + translate_region(region) + "/" + user_id,
+                                query: {api_key: key}.to_query)
+    json = HTTParty.get(game_uri, verify: false)
+    render json: json["gameId"] ? json_in_game : json_out_game
+  end
+
+  def get_user_id(region, username)
+    key = next_key
+    id_uri = URI::HTTPS.build(host: region + @@request_base,
+                              path: @@id_path1 + region + @@id_path2 + username,
+                              query: {api_key: key}.to_query)
+    json = HTTParty.get(id_uri, verify: false)
+    json[username.downcase]["id"].to_s if json[username.downcase]
 
   end
 
   def json_in_game
-    {in_game: true}
+    {valid: true, in_game: true}
   end
-  def json_out_game
-    {in_game: false}
 
+  def json_out_game
+    {valid: true, in_game: false}
   end
+
+  def json_invalid_summoner
+    {valid: false, in_game: true}
+  end
+
+  def next_key
+    @@key_index += 1
+    @@key_index = 0 if @@key_index > @@keys.length
+    @@keys[@@key_index]
+  end
+
+  def translate_region(region)
+    @@region_translator[region.to_sym]
+  end
+
 end
