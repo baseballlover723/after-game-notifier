e9b9eafccebe90f1c0cd131a1752120bdae80d96
Philip Ross <rosspa@rose-hulman.edu>
2016-03-17 00:12:41 -0400

    optimized queue

--- lib/queue_proxy.rb
+++ lib/queue_proxy.rb
@@ -4,46 +4,30 @@ class QueueProxy < Queue
     @count = 0
   end
 
-  def slow_pop(id)
-    count = @count
-    @last_slow = true
+  def slow_pop
     result = pop
-    # puts @count
     if @count != 0
-      # puts "result: #{result}"
-      # (num_waiting).times do
-      #   puts "num waiting: #{num_waiting}"
       #   puts "go again #{id}"
       Thread.new do
         push result
       end
-      # puts size
       result = pop
       @count = 0
-      # puts "result: #{result}"
-      #   puts "slow pop #{id}"
-      # end
-      # puts "#{@count}*"
     end
-    # puts "after slow pop #{@count}"
     result
   end
 
   def fast_pop
-    @count += 1 if @last_slow
-    @last_slow = false
-    result = pop
-    # puts "after fast"
-    @count -= 1
-    result
+    @count += 1
+    pop
   end
 end
 
 # TEST
 blah = QueueProxy.new
-test_numb = 100
+test_numb = 10
 Thread.new do
-  sleep 2
+  sleep 1
   (1..test_numb).each do |numb|
     blah << numb
     sleep 0.1
@@ -61,7 +45,7 @@ arr.each do |do_fast|
     if do_fast
       puts "fast pop #{count}: #{blah.fast_pop}"
     else
-      puts "normal #{count}: #{blah.slow_pop count}"
+      puts "normal #{count}: #{blah.slow_pop}"
     end
   end
 end
